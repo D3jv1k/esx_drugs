31493b1e365c6cdf5f8df4233d3236c6fc9fe050
David <101667551+v4mp76@users.noreply.github.com>
2022-06-06 14:50:42 +0200

    Update es.lua

--- locales/es.lua
+++ locales/es.lua
@@ -32,21 +32,7 @@ Locales ['es'] = {
   ['coke_processingenough'] = 'Debes tener ~b~3x~s~ ~g~Hoja De Coca~s~ encima para procesar.',
   ['coke_processed'] = 'Has procesado ~b~1x~s~ ~g~Cocaína~s~',
   ['coke_processingtoofar'] = 'El procesado ha sido ~r~cancelado~s~ porque te has alejado demasiado.',
-  
-  -- lsd
-  ['lsd_processingstarted'] = 'Procesando ~g~LSD~s~...',
-  ['lsd_processingfull'] = 'Procesado ~r~cancelado~s~ ¡porque tienes el inventario lleno!',
-  ['lsd_processprompt'] = 'Presiona ~INPUT_CONTEXT~ para comenzar a ~gProcesar LSD~s~.',
-  ['lsd_processingenough'] = 'Debes tener ~b~1x~s~ ~g~LSA~s~ y ~b~1x~s~ ~g~Cloruro De Tionilo~s~ encima para procesar.',
-  ['lsd_processed'] = 'Has procesado ~b~1x~s~ ~g~LSD~s~',
-  ['lsd_processingtoofar'] = 'El procesado ha sido ~r~cancelado~s~ porque te has alejado demasiado.',
-  
-  ['thionylchloride_processingstarted'] = 'Procesando ~g~Cloruro De Tionilo~s~...',
-  ['thionylchloride_processingfull'] = 'Procesado ~r~cancelado~s~ ¡porque tienes el inventario lleno!',
-  ['thionylchloride_processprompt'] = 'Presiona ~INPUT_CONTEXT~ para comenzar a ~g~Procesar Cloruro De Tionilo~s~.',
-  ['thionylchloride_processingenough'] = 'Debes tener ~b~1x~s~ ~g~LSA~s~ y ~b~1x~s~ ~g~Productos Quimicos~g~ encima para procesar.',
-  ['thionylchloride_processed'] = 'Has procesado ~b~1x~s~ ~g~Cloruro De Tionilo~s~',
-  ['thionylchloride_processingtoofar'] = 'El procesado ha sido ~r~cancelado~s~ porque te has alejado demasiado.',
+ 
   
    -- heroin
   ['heroin_pickupprompt'] = 'Presiona ~INPUT_CONTEXT~ para cosechar el cogollo de la ~g~Amapola~s~.',
@@ -58,22 +44,6 @@ Locales ['es'] = {
   ['heroin_processingenough'] = 'Debes tener ~b~1x~s~ ~g~Resina De Amapola~s~ encima para procesar.',
   ['heroin_processed'] = 'Has procesado ~b~1x~s~ ~g~Heroína~s~',
   ['heroin_processingtoofar'] = 'El procesado ha sido ~r~cancelado~s~ porque te has alejado demasiado.',
-  
-  -- chemicals
-  ['chemicals_pickupprompt'] = 'Presiona ~INPUT_CONTEXT~ para recuperar los ~g~Productos Quimicos~g~',
-  ['chemicals_inventoryfull'] = 'No tienes más espacio de inventario para ~g~Productos Quimicos~g~.',
-  ['Chemicals_notenough'] = 'No tienes mas ~g~Productos Quimicos~g~ para procesar.',
-  ['Chemicals_made'] = 'Usted ha hecho ~b~1x~s~ ~y~%s~s~',
-  ['chemicals_prompt'] = 'Presiona ~INPUT_CONTEXT~ para abrir el ~r~Menú De Conversión Química~s~.',
-  ['ChemicalsConvertion_title'] = 'Menú De Conversión Química',
-  ['chemicals_making'] = '~g~Fabricación...~s~',
-  
-  -- drug dealer
-  ['dealer_prompt'] = 'Presiona ~INPUT_CONTEXT~ para hablar con el ~r~Narcotraficante~s~.',
-  ['dealer_title'] = 'Narcotraficante',
-  ['dealer_item'] = '$%s',
-  ['dealer_notenough'] = '¡No tienes suficiente droga para vender!',
-  ['dealer_sold'] = 'Has vendido ~b~%sx~s~ ~y~%s~s~ por ~g~$%s~s~',
 
   -- license
   ['license_title'] = 'Debes tener una licencia de procesado para procesar este producto, se puede comprar aquí.',
@@ -93,11 +63,5 @@ Locales ['es'] = {
   ['moneywash_title'] = 'Lavado de dinero',
   ['moneywash_wash'] = 'Lava todo el dinero negro.',
   ['moneywash_prompt'] = 'Presiona ~INPUT_CONTEXT~ para abrir el ~r~Menu de Lavado de dinero~s~.',
-  
-  -- blips
-  ['blip_weedfield'] = 'Campo de cannabis',
-  ['blip_weedprocessing'] = 'Tratamiento de Cannabis',
-  ['blip_methfield'] = 'Campo de Metanfetamina',
-  ['blip_methprocessing'] = 'Tratamiento de Metanfetamina',
-  ['blip_drugdealer'] = 'Narcotraficante',
+ 
 }
