55416e9937d2e1b3ba379c1020d11030b39e0e60
David <101667551+v4mp76@users.noreply.github.com>
2022-06-06 14:51:44 +0200

    Update sv.lua

--- locales/sv.lua
+++ locales/sv.lua
@@ -9,22 +9,10 @@ Locales ['sv'] = {
   ['weed_processed'] = 'du har processerat ~b~3x~s~ ~g~Cannabis~s~ till ~b~1x~s~ ~g~Marijuana~s~',
   ['weed_processingtoofar'] = 'processeringen har ~r~har avbrutit~s~ på grund av att du lämnade området.',
 
-  -- drug dealer
-  ['dealer_prompt'] = 'tryck ~INPUT_CONTEXT~ för att prata med ~r~Drågsäljaren~s~.',
-  ['dealer_title'] = 'drogsäljare',
-  ['dealer_item'] = '%s SEK',
-  ['dealer_notenough'] = 'du har inte tillräckligt med det till salu!',
-  ['dealer_sold'] = 'du har sålt ~b~%sx~s~ ~y~%s~s~ för ~g~%s SEK~s~',
-
   -- license
   ['license_title'] = 'du måste ha ett visst processeringslicens för att kunna bearbeta denna produkt, detta kan dock köpas här.',
   ['license_no'] = 'nej',
   ['license_bought'] = 'du köpte ~b~%s~s~ för ~r~%s SEK~s~',
   ['license_bought_fail'] = 'du har inte råd för ~b~%s~s~!',
   ['license_weed'] = 'marijuana processeringslicens',
-
-  -- blips
-  ['blip_weedfield'] = 'marijuana plock',
-  ['blip_weedprocessing'] = 'marijuana processering',
-  ['blip_drugdealer'] = 'drogsäljare',
 }
