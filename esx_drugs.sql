6f844759f1a7008e8a64f7b1d74ac478262ac598
David <101667551+v4mp76@users.noreply.github.com>
2022-06-06 14:48:43 +0200

    Update esx_drugs.sql

--- esx_drugs.sql
+++ esx_drugs.sql
@@ -1,7 +1,7 @@
 INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
 	('cannabis', 'Cannabis', 1, 0, 1),
-	('marijuana', 'Marijuana', 1, 0, 1),
-	('coca_leaf', 'Koksblatt', 1, 0, 1),
+	('marijuana', 'Marihuana', 1, 0, 1),
+	('coca_leaf', 'cocain leaf', 1, 0, 1),
 	('coke', 'Cocain', 1, 0, 1),
 	('poppyresin', 'Poppyresin', 1, 0, 1),
 	('heroin', 'Heroin', 1, 0, 1),
