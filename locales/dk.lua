ecc7a8f5e4d6d05c08e28a622721072bab979e43
David <101667551+v4mp76@users.noreply.github.com>
2022-06-06 14:51:29 +0200

    Update dk.lua

--- locales/dk.lua
+++ locales/dk.lua
@@ -9,22 +9,10 @@ Locales ['dk'] = {
     ['weed_processed'] = 'du har lavet ~b~2 stks~s~ ~g~Cannabis~s~ om til ~b~1 stks~s~ ~g~Marihuana~s~',
     ['weed_processingtoofar'] = 'din behandling er blevet ~r~annulleret~s~ fordi du kom ud af rækkevidde.',
 
-    -- drug dealer
-    ['dealer_prompt'] = 'tryk ~INPUT_CONTEXT~ for at snakke med ~r~Pusheren~s~.',
-    ['dealer_title'] = 'Pusher',
-    ['dealer_item'] = '$%s',
-    ['dealer_notenough'] = 'Du har ikke nok til at sælge!',
-    ['dealer_sold'] = 'Du har solgt ~b~%sx~s~ ~y~%s~s~ for ~g~$%s~s~',
-
     -- license
     ['license_title'] = 'Du er forpligtet til at eje et behandlingslicens for at lave stoffer, det kan købes her.',
     ['license_no'] = 'nej',
     ['license_bought'] = 'du købte ~b~%s~s~ for ~r~$%s~s~',
     ['license_bought_fail'] = 'du har ikke råd til en ~b~%s~s~!',
     ['license_weed'] = 'Cannabis behandlings licens',
-
-    -- blips
-    ['blip_weedfield'] = 'Cannabis Mark',
-    ['blip_weedprocessing'] = 'Cannabis Behandlingscenter',
-    ['blip_drugdealer'] = 'Pusher',
 }
